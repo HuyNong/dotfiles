0d1829026032ea1932f0655692551547c237b1c7
Huy Nong <huynd@emddi.com>
2024-02-05 17:58:26 +0700

    feat<nvim>: addition to telescope

--- nvim/huynd/plugins/telescope.lua
+++ nvim/huynd/plugins/telescope.lua
@@ -1,6 +1,10 @@
 return {
     'nvim-telescope/telescope.nvim', tag = '0.1.5',
-    dependencies = { 'nvim-lua/plenary.nvim' },
+    dependencies = {
+        'nvim-lua/plenary.nvim',
+        { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+        "nvim-tree/nvim-web-devicons",
+    },
 
     config = function()
         local builtin = require('telescope.builtin')
@@ -8,11 +12,14 @@ return {
         vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope: Search live grep'})
         vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope: Find in buffers'})
         vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope: Find helps'})
+        vim.keymap.set('n', '<leader>fr', builtin.oldfiles, { desc = 'Telescope: Find recent files'})
+
 
         local actions = require("telescope.actions")
         require("telescope").setup({
             defaults = {
                 prompt_prefix = "   ",
+                path_display = { "truncate " },
                 sorting_strategy = "ascending",
                 mappings = {
                     i = {
