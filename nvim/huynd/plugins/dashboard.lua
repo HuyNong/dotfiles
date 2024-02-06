8c8310f31369ef0e49593e2514fd116573164ee1
Huy Nong <huynd@emddi.com>
2024-02-06 18:32:28 +0700

    feat<nvim>: add dashboard, surround, comment

---
+++ nvim/huynd/plugins/dashboard.lua
@@ -0,0 +1,30 @@
+return {
+    'nvimdev/dashboard-nvim',
+    event = 'VimEnter',
+    config = function()
+        require('dashboard').setup({
+            theme = 'hyper',
+            shortcut_type = 'number',
+            config = {
+                week_header = {
+                    enable = true,
+                },
+                shortcut = {
+                    { desc = '󰊳 Update', group = '@property', action = 'Lazy update', key = 'u' },
+                    {
+                        icon = ' ',
+                        icon_hl = '@variable',
+                        desc = 'Files',
+                        group = 'Label',
+                        action = 'Telescope find_files',
+                        key = 'f',
+                    },
+                },
+                project = { enable = false },
+                mru = { limit = 10, icon = ' ', label = 'Recent files', cwd_only = true },
+                footer = {}, -- footer
+            },
+        })
+    end,
+    dependencies = { {'nvim-tree/nvim-web-devicons'}}
+}
