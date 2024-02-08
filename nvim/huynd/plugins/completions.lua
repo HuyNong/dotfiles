33e01aec67cb888e6e19d1ae37505fc040aec313
Huy Nong <huynd@emddi.com>
2024-02-08 10:08:23 +0700

    feat(nvim): add tabnine completion, modify key bindings

--- nvim/huynd/plugins/completions.lua
+++ nvim/huynd/plugins/completions.lua
@@ -27,13 +27,42 @@ return {
                     ["<C-e>"] = cmp.mapping.abort(),
                     ["<CR>"] = cmp.mapping.confirm { select = true }, -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
                 },
-                sources = cmp.config.sources({
-                    -- { name = 'nvim_lsp' },
+                sources = cmp.config.sources {
+                    { name = "nvim_lsp" },
                     { name = "luasnip" }, -- For luasnip users.
-                }, {
                     { name = "buffer" },
-                }),
+                    { name = "cmp_tabnine" },
+                },
+            }
+
+            local compare = require "cmp.config.compare"
+            require("cmp").setup {
+                sorting = {
+                    priority_weight = 2,
+                    comparators = {
+                        require "cmp_tabnine.compare",
+                        compare.offset,
+                        compare.exact,
+                        compare.score,
+                        compare.recently_used,
+                        compare.kind,
+                        compare.sort_text,
+                        compare.length,
+                        compare.order,
+                    },
+                },
             }
         end,
+
+        dependencies = {
+            {
+                "tzachar/cmp-tabnine",
+                build = "./install.sh",
+                config = function()
+                    local tabnine = require "cmp_tabnine.config"
+                    tabnine:setup {} -- put your options here
+                end,
+            },
+        },
     },
 }
