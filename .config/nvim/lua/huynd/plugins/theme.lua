e20d5ad4f28b07bf6b218062440a87ef7145ca3d
Huy Nong <huynd@emddi.com>
2024-03-13 09:05:13 +0700

    feat(nvim): change theme

--- .config/nvim/lua/huynd/plugins/theme.lua
+++ .config/nvim/lua/huynd/plugins/theme.lua
@@ -1,28 +1,40 @@
 return {
-    {
-        "catppuccin/nvim",
-        priority = 1000,
-        name = "catppuccin",
-        config = function()
-            require("catppuccin").setup {
-                flavour = "mocha",
-                transparent_background = true,
-            }
-            vim.cmd.colorscheme "catppuccin"
-        end,
-    },
-    {
-        "nvim-lualine/lualine.nvim",
-        dependencies = { "nvim-tree/nvim-web-devicons" },
-        config = function()
-            require("lualine").setup {
-                sections = {
-                    lualine_b = { "branch", "diff" },
-                    lualine_x = { "diagnostics" },
-                    lualine_y = {},
-                    lualine_z = { "location", "progress" },
-                },
-            }
-        end,
-    },
+  -- {
+  --     "catppuccin/nvim",
+  --     priority = 1000,
+  --     name = "catppuccin",
+  --     config = function()
+  --         require("catppuccin").setup {
+  --             flavour = "mocha",
+  --             transparent_background = true,
+  --         }
+  --         vim.cmd.colorscheme "catppuccin"
+  --     end,
+  -- },
+  {
+    "ellisonleao/gruvbox.nvim",
+    priority = 1000,
+    config = function(opts)
+      require("gruvbox").setup({
+        transparent_mode = true,
+      })
+
+      vim.o.background = "dark" -- or "light" for light mode
+      vim.cmd([[colorscheme gruvbox]])
+    end,
+  },
+  {
+    "nvim-lualine/lualine.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+      require("lualine").setup {
+        sections = {
+          lualine_b = { "branch", "diff" },
+          lualine_x = { "diagnostics" },
+          lualine_y = {},
+          lualine_z = { "location", "progress" },
+        },
+      }
+    end,
+  },
 }
