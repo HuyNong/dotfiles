8dd23ae8c08878b820c639d4ce165c9c2ee76389
Huy Nong <huynd@emddi.com>
2024-03-22 11:18:08 +0700

    feat(nvim): golang

--- .config/nvim/lua/huynd/plugins/init.lua
+++ .config/nvim/lua/huynd/plugins/init.lua
@@ -86,4 +86,23 @@ return {
   {
     "folke/twilight.nvim",
   },
+  {
+    "olexsmir/gopher.nvim",
+    ft = "go",
+    requires = { -- dependencies
+      "nvim-lua/plenary.nvim",
+      "nvim-treesitter/nvim-treesitter",
+    },
+    config = function()
+      require("gopher").setup({
+        commands = {
+          go = "go",
+          gomodifytags = "gomodifytags",
+          gotests = "~/go/bin/gotests", -- also you can set custom command path
+          impl = "impl",
+          iferr = "iferr",
+        },
+      })
+    end,
+  },
 }
--- .config/nvim/lua/huynd/plugins/none-ls.lua
+++ .config/nvim/lua/huynd/plugins/none-ls.lua
@@ -1,16 +1,25 @@
 return {
   "nvimtools/none-ls.nvim",
   config = function()
-    local null_ls = require "null-ls"
+    local null_ls = require("null-ls")
 
-    null_ls.setup {
+    null_ls.setup({
       sources = {
         null_ls.builtins.formatting.stylua,
         null_ls.builtins.completion.spell,
+
+        -- js/ts
         null_ls.builtins.diagnostics.eslint_d,
         null_ls.builtins.formatting.prettierd,
+
+        -- php
         null_ls.builtins.formatting.phpcsfixer,
+
+        -- go
+        null_ls.builtins.formatting.gofumpt,
+        null_ls.builtins.formatting.goimports_reviser,
+        null_ls.builtins.formatting.golines,
       },
-    }
+    })
   end,
 }
