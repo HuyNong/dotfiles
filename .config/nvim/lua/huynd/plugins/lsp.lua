8966c8381f366b3319ea89c60eeaf1b4fb6fc7f0
Huy Nong <nongduchuy95@gmail.com>
2024-06-06 13:19:46 +0700

    chore(nvim): lsp add default install

--- .config/nvim/lua/huynd/plugins/lsp.lua
+++ .config/nvim/lua/huynd/plugins/lsp.lua
@@ -9,7 +9,7 @@ return {
     "williamboman/mason-lspconfig.nvim",
     config = function()
       require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "html", "cssls", "tsserver", "intelephense", "gopls" },
+        ensure_installed = { "lua_ls", "html", "cssls", "tsserver", "intelephense", "php-cs-fixer", "gopls" },
       })
     end,
   },
