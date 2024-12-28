e3283a5d8295d671cf0f449834268a43a41fde28
Huy Nong <huynong@emddi.com>
2024-12-29 01:02:48 +0700

    feat(nvim): correct use of stow on nvim

--- nvim/.config/nvim/lua/huynd/plugins/terminal.lua
+++ nvim/.config/nvim/lua/huynd/plugins/terminal.lua
@@ -4,6 +4,7 @@ return {
     vim.g.floaterm_width = 0.8
     vim.g.floaterm_height = 0.8
     vim.g.floaterm_titleposition = 'center'
+    vim.g.floaterm_shell = 'zsh'
 
     vim.api.nvim_set_keymap("n", "<leader>tn", "<CMD>FloatermNew<CR>", { desc = "Term: New float terminal" })
     vim.api.nvim_set_keymap("t", "<leader>tn", "<CMD>FloatermNew<CR>", { desc = "Term: New float terminal" })
